e8aadf717fb363a588f4fa11eceea07ac4583bb0
FeryET <farhoodetaati@ut.ac.ir>
2021-03-29 16:32:38 +0430

    fixes tests for layer/activation/weight modules

--- core/tests/test.py
+++ core/tests/test.py
@@ -1,6 +1,3 @@
-import sys
-sys.path.append("..")
-
 from core.nnlib import *
 import unittest
 import numpy as np
@@ -26,16 +23,16 @@ class TestActivations:
         leakage = 0.1
         relu = LeakyRelu(leakage_ratio=leakage)
         ntest.assert_array_equal(
-            np.array([i if i >= 0 else leakage for i in X],
+            np.array([1 if i >= 0 else leakage for i in X],
                      dtype=np.float32),
             relu.derivative(X))
-        X = np.zeros((10, 10), np.float32)
-        ntest.assert_array_equal(np.zeros((10, 10)), relu.derivative(X))
+        X = np.ones((10, 10), np.float32)
+        ntest.assert_array_equal(X, relu.derivative(X))
 
 
 class TestWeights:
     def test_abstract_weight_initializer(self):
-        ntest.assert_raises(TypeError, WeightInitializer())
+        ntest.assert_raises(TypeError, WeightInitializer)
 
     def test_normal_weight_initializer(self):
         shape = (100, 1000)
@@ -47,15 +44,15 @@ class TestWeights:
 
 class TestLayers:
     def test_abstract_layer(self):
-        ntest.assert_raises(TypeError, Layer())
+        ntest.assert_raises(TypeError, Layer)
 
     def test_dense_layer(self):
-        shape = (10, 100)
-        l = DenseLayer(*shape, LeakyRelu())
-        X = np.random.normal(0, 2, (100, shape[0]))
-        ntest.assert_equal(shape[1], l(X).shape[1])
+        shape = (10, 100)  # 10 features, 1000 elements
+        l = DenseLayer(input_dim=shape[0], output_dim=shape[1], activation=LeakyRelu())
+        X = np.random.normal(0, 2, (shape[0], 1000))
+        # transposing because the feature vector is deemed to be feature first
+        ntest.assert_equal(shape[1], l(X).T.shape[1])
 
 
 if __name__ == "__main__":
-    # unittest.TestResult
     pass
