ea4aa2a2ef4f8e7ee4ca7f33eac1fe03202e3021
FeryET <farhoodetaati@ut.ac.ir>
2021-03-29 17:55:43 +0430

    fixes abstract class issues

--- core/nnlib/decay.py
+++ core/nnlib/decay.py
@@ -2,10 +2,12 @@ import numpy as np
 from abc import ABC, abstractmethod
 
 #Defining base lr decay class
-class LearningRateDecay():
+class LearningRateDecay(ABC):
     def __init__(self, *args, **kwargs):
       pass
-
+    @abstractmethod
+    def __call__(self, *args, **kwargs):
+      pass
 class ConstantDecay(LearningRateDecay):
   def __init__(self, decay=0.97):
     LearningRateDecay.__init__(self)
